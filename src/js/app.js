79924403ccf66edad210e6dac785d2b30c44e1d7
Paul M. Christian <paulmchristian@gmail.com>
2016-02-11 16:43:09 -0600

    cleanup

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ import 'babel-polyfill';
 import {Animation, Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import OSC from 'osc/dist/osc-browser';
 
 import rgbToHex from './util/util.js';
 import Camera from './components/Camera';
@@ -11,32 +10,9 @@ import Cursor from './components/Cursor';
 import Light from './components/Light';
 import Sky from './components/Sky';
 
-class Temple extends React.Component {
-  render() {
-    return (
-
-      <Entity loader={{src: 'url(models/model_scene-temple_04.dae)', format: 'collada'}}
-              rotation={`0 180 0`}
-              position={this.props.position}>
-      </Entity>
-    );
-  }
-}
-
-class Cloud extends React.Component {
-  render() {
-    const {scale} = this.props;
-    return (
-      <Entity loader={{src: 'url(models/model_cloud_01.dae)', format: 'collada'}}
-              material={{color: '#fff', transparent: true, opacity: 0.2}}
-              rotation={`0 180 0`}
-              scale={`${scale} ${scale} ${scale}`}
-              position={this.props.position}
-      >
-      </Entity>
-    );
-  }
-}
+THREE.Vector3.prototype.toAframeString = function() {return `${this.x} ${this.y} ${this.z}`};
+window.V3 = (x, y, z) => new THREE.Vector3(x, y, z);
+window.V3toStr = (x, y, z) => V3(x, y, z).toAframeString();
 
 class Paddle extends React.Component {
   render() {
@@ -76,19 +52,6 @@ class Ball extends React.Component {
   }
 }
 
-class Pedal extends React.Component {
-  render() {
-    return (
-      <Entity rotation={`${-30 + 60 * this.props.depression} 0 0`} position={this.props.position}>
-        <Entity geometry="primitive: box; width: 1.5; height: 0.5; depth: 2.5"
-                material={{color: '#555', metalness: 1}}
-                position="0 0 3"
-        />
-      </Entity>
-    );
-  }
-}
-
 export class App extends React.Component {
   constructor(props) {
     super(props);
@@ -115,11 +78,7 @@ export class App extends React.Component {
       },
       ball: {x: 0, y: 0, z: -5, r: 0.25, rotation: 0},
       velocity: {x: 3, y: 0, z: 3},
-      t: 0,
-      osc: {acc: {}},
       keys: { 38: false, 40: false },
-      elevation: 3,
-      elevationVel: 0,
       lightColor: "#0f0"
     }
   }
@@ -127,41 +86,8 @@ export class App extends React.Component {
   componentDidMount() {
     window.app = this;
 
-    let c = ReactDOM.findDOMNode(this.refs.camera);
-
     window.addEventListener('keydown', this.handleKeyDown.bind(this));
     window.addEventListener('keyup', this.handleKeyUp.bind(this));
-
-    this.oscPort = new OSC.WebSocketPort({
-      url: `ws://${window.location.hostname}:8081`
-    });
-
-    this.oscPort.on("message", (msg) => {
-      let {osc, paddle1, paddle2} = this.state;
-
-      if (msg.address == '/1/fader1') {
-        osc.f1 = msg.args[0];
-      }
-
-      if (msg.address == '/1/fader4') {
-        osc.f4 = msg.args[0];
-      }
-
-      if (msg.address == '/stepper') {
-        osc.stairmaster = msg.args[0];
-      }
-
-      if (msg.address == '/accxyz') {
-        osc.acc = {x: msg.args[0], y: msg.args[2], z: msg.args[1]};
-      }
-
-      paddle1.pos.z = (osc.stairmaster - 0.5) * this.state.arena.width;
-      paddle2.pos.z = (1 - osc.stairmaster - 0.5) * this.state.arena.width;
-
-      this.setState({osc, paddle1, paddle2});
-    });
-
-    this.oscPort.open();
   }
 
   handleKeyDown(e) {
@@ -173,10 +99,12 @@ export class App extends React.Component {
   }
 
   tick() {
+    // timing
     const prevMillis = this.millis || Date.now();
     this.millis = Date.now();
     let dt_seconds = (this.millis - prevMillis) / 1000;
 
+    // ray casting
     let c = ReactDOM.findDOMNode(this.refs.camera);
     const p = c.components.camera.camera.getWorldPosition();
     const d = c.components.camera.camera.getWorldDirection();
@@ -184,7 +112,7 @@ export class App extends React.Component {
     const plane = new THREE.Plane(new THREE.Vector3(-1, 0, 0), this.state.arena.width / 2);
     const paddleZ = ray.intersectPlane(plane).z;
 
-    let {t, velocity, ball, paddle1, paddle2, arena, elevation, elevationVel, keys} = this.state;
+    let {velocity, ball, paddle1, paddle2, arena, keys} = this.state;
     let {x, y, z, r, rotation} = ball;
 
     let isCollision = paddle => Math.abs(paddle.pos.z - ball.z) < (paddle.width / 2 + ball.r);
@@ -236,7 +164,6 @@ export class App extends React.Component {
     let out_of_arena = Math.abs(ball.x) > 23/2;
     if (out_of_arena) {
       this.state.lightColor = "#0f0";
-      this.state.elevationVel += 0.07;
       this.state.ball = {x: 0, y, z, r, rotation};
     } else {
       let out_of_bounds = Math.abs(ball.x) > arena.width/2;
@@ -253,8 +180,6 @@ export class App extends React.Component {
 
       this.state.t += dt_seconds;
       this.state.velocity = velocity;
-      this.state.elevationVel *= 0.95;
-      this.state.elevation += elevationVel;
     }
 
     this.forceUpdate();
@@ -262,8 +187,7 @@ export class App extends React.Component {
 
   render() {
     let {x, y, z, r, rotation} = this.state.ball;
-    let {acc, stairmaster} = this.state.osc;
-    let {paddle1, paddle2, elevation} = this.state;
+    let {paddle1, paddle2} = this.state;
 
     return (
       <Scene onTick={this.tick}>
@@ -271,13 +195,6 @@ export class App extends React.Component {
           <Entity ref="camera" camera wasd-controls look-controls>
             <Cursor />
           </Entity>
-
-          { !stairmaster ?
-            <Entity position={`0 -10 -1`}>
-              <Pedal position="-1 0 0" depression={this.state.osc.stairmaster}/>
-              <Pedal position="1 0 0" depression={1 - this.state.osc.stairmaster}/>
-            </Entity> : null
-          }
         </Entity>
 
         <Sky/>
@@ -286,13 +203,6 @@ export class App extends React.Component {
         <Light type="ambient" color="#666"/>
         <Light type="point" intensity="1" color="#766"/>
         <Light type="point" intensity="2" distance="20" color={this.state.lightColor}/>
-
-        <Temple position={`0 -40 56`}/>
-
-        <Cloud position={`4 80 3`} scale={2} />
-        <Cloud position={`-5 20 -2`} scale={0.5} />
-        <Cloud position={`7 30 -5`} scale={0.5} />
-        <Cloud position={`7 50 -5`} scale={0.5} />
 
         <Arena position={`0 -0.4 0`} width={this.state.arena.width} height={0.1} depth={this.state.arena.depth} />
 
@@ -307,8 +217,7 @@ export class App extends React.Component {
                 depth={paddle2.depth}
                 color="#aaf"/>
 
-        <Ball position={`${x} ${y} ${z}`} rotation={`0 ${rotation} 0`} radius={r}
-              color={rgbToHex(128 + acc.x * 12.8, 128 + acc.y * 12.8, 128 + acc.z * 12.8)}/>
+        <Ball position={`${x} ${y} ${z}`} rotation={`0 ${rotation} 0`} radius={r}/>
       </Scene>
     );
   }
